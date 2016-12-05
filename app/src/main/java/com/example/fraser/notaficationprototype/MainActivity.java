a39e7a5c582e8ced52d90048b43cd4333b07e202
fraser0319 <fraser0319@gmail.com>
2016-12-05 20:22:01 +0000

    added functionality to get all buttons cycling through images although i think im missing something silly, will have a look later on

--- app/src/main/java/com/example/fraser/notaficationprototype/MainActivity.java
+++ app/src/main/java/com/example/fraser/notaficationprototype/MainActivity.java
@@ -16,6 +16,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     private static NotificationManager manager;
     private static RemoteViews contentView;
     private static int emotionCounter = 0;
+    private static int authennCounter = 0;
+    private static int deviceCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,22 +40,45 @@ public class MainActivity extends AppCompatActivity {
         t.show();
     }
 
-    public void setupButtons(){
-        contentView.setImageViewResource(R.id.emotionButton,R.drawable.happy);
+    public void setupButtons() {
+        contentView.setImageViewResource(R.id.emotionButton, R.drawable.happy);
+        contentView.setImageViewResource(R.id.authenticatorButton, R.drawable.fingerprintscan);
+        contentView.setImageViewResource(R.id.deviceButton, R.drawable.smartphone);
     }
 
     @SuppressWarnings("deprecation")
-    public void sendNotification(){
+    public void sendNotification() {
 
-        Intent notificationIntent = new Intent(this,MainActivity.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(this,0,notificationIntent,0);
+        List<Integer> emotionList = new ArrayList<Integer>();
+        Collections.addAll(emotionList, R.drawable.sad, R.drawable.confused, R.drawable.happy);
 
-        contentView = new RemoteViews(getPackageName(),R.layout.notification_view);
+        List<Integer> authenList = new ArrayList<Integer>();
+        Collections.addAll(authenList, R.drawable.sad, R.drawable.confused, R.drawable.happy);
+
+        List<Integer> devList = new ArrayList<Integer>();
+        Collections.addAll(devList , R.drawable.car,R.drawable.metro,R.drawable.smartphone);
+
+
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        contentView = new RemoteViews(getPackageName(), R.layout.notification_view);
         contentView.setTextViewText(R.id.title, "title");
 
         // add intents for each button to rotate through each image.
         Intent changeEmotion = new Intent("changeEmotion");
-        PendingIntent pendingChangeEmotion = PendingIntent.getBroadcast(this,0,changeEmotion,0);
+        changeEmotion.putIntegerArrayListExtra("emoImgList", (ArrayList<Integer>) emotionList);
+        PendingIntent pendingChangeEmotion = PendingIntent.getBroadcast(this, 0, changeEmotion, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Intent changeAuthen = new Intent("changeAuthen");
+        changeEmotion.putIntegerArrayListExtra("authenImgList", (ArrayList<Integer>) authenList);
+        PendingIntent pendingChangeAuthen = PendingIntent.getBroadcast(this, 0, changeAuthen, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Intent changeDevice = new Intent("changeDevice");
+        changeEmotion.putIntegerArrayListExtra("devImgList", (ArrayList<Integer>) devList);
+        PendingIntent pendingChangeDevice  = PendingIntent.getBroadcast(this, 0, changeDevice , PendingIntent.FLAG_UPDATE_CURRENT);
+
         // build the notification
         notification = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.diary)
@@ -62,31 +88,57 @@ public class MainActivity extends AppCompatActivity {
                 .setOngoing(true)
                 .setContentTitle("custom View").build();
         // add onclickpending intent listeners for each button
-        contentView.setOnClickPendingIntent(R.id.emotionButton,pendingChangeEmotion);
+        contentView.setOnClickPendingIntent(R.id.emotionButton, pendingChangeEmotion);
+        contentView.setOnClickPendingIntent(R.id.authenticatorButton, pendingChangeAuthen);
+        contentView.setOnClickPendingIntent(R.id.deviceButton, pendingChangeDevice );
         setupButtons();
-        Log.e("here","here");
+        Log.e("here", "here");
         notification.contentView = contentView;
         manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        manager.notify(1,notification);
+        manager.notify(1, notification);
     }
 
     public static class ChangeEmotionRecevicer extends BroadcastReceiver {
 
-        @SuppressWarnings("deprecation")
         @Override
         public void onReceive(Context context, Intent intent) {
-            Log.e("reciverAction",intent.getAction());
-            Integer [] images = new Integer[]{R.drawable.sad,R.drawable.confused,R.drawable.happy,};
-            List<Integer> imageList = Arrays.asList(images);
-            if(emotionCounter < imageList.size()){
-                imageList.get(emotionCounter);
-                contentView.setImageViewResource(R.id.emotionButton,imageList.get(emotionCounter));
-                emotionCounter++;
-            }else {
-                emotionCounter = 0;
+            Log.e("getAction",intent.getAction());
+            switch (intent.getAction()) {
+                case "changeEmotion":
+                    ArrayList<Integer> sentList = intent.getIntegerArrayListExtra("emoImgList");
+                    if (emotionCounter < sentList.size()) {
+                        updateButton(sentList, emotionCounter,R.id.emotionButton);
+                        emotionCounter++;
+                    } else {
+                        emotionCounter = 0;
+                    }
+                    break;
+                case "changeAuthentication":
+                    ArrayList<Integer> authenImgList= intent.getIntegerArrayListExtra("authenImgList");
+                    if (authennCounter < authenImgList.size()) {
+                        updateButton(authenImgList, authennCounter,R.id.authenticatorButton);
+                        authennCounter++;
+                    } else {
+                        authennCounter = 0;
+                    }
+                    break;
+                case "changeDevice":
+                    ArrayList<Integer> devImgList = intent.getIntegerArrayListExtra("devImgList");
+                    if (deviceCounter < devImgList.size()) {
+                        updateButton(devImgList, deviceCounter,R.id.deviceButton);
+                        deviceCounter++;
+                    } else {
+                        deviceCounter = 0;
+                    }
+                    break;
             }
+        }
+
+        @SuppressWarnings("deprecation")
+        public void updateButton(ArrayList<Integer> imageList, int counter,int buttonID) {
+            contentView.setImageViewResource(buttonID, imageList.get(counter));
             notification.contentView = contentView;
-            manager.notify(1,notification);
+            manager.notify(1, notification);
         }
     }
 }
