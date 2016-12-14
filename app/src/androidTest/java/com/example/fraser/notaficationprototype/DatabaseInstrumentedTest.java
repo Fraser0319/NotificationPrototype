d0279d7717ae4d0d036e6320d0c2e36e642c7cdc
fraser0319 <fraser0319@gmail.com>
2016-12-14 16:18:27 +0000

    got the database tests working and its inserting data into the table

--- app/src/androidTest/java/com/example/fraser/notaficationprototype/DatabaseInstrumentedTest.java
+++ app/src/androidTest/java/com/example/fraser/notaficationprototype/DatabaseInstrumentedTest.java
@@ -1,17 +1,21 @@
 package com.example.fraser.notaficationprototype;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static junit.framework.Assert.assertEquals;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.R.attr.id;
+import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertNotNull;
 
 
@@ -22,19 +26,20 @@ import static junit.framework.Assert.assertNotNull;
 public class DatabaseInstrumentedTest {
 
     private DatabaseHelper mHelper;
-    private SQLiteDatabase mDatabase;
+    private SQLiteDatabase mWritableDatabase;
+    private SQLiteDatabase mReadableDatabase;
 
     @Before
     public void setUp() throws Exception {
         mHelper = new DatabaseHelper(InstrumentationRegistry.getTargetContext());
-        mDatabase = mHelper.getReadableDatabase();
-
+        mReadableDatabase = mHelper.getReadableDatabase();
+        mWritableDatabase = mHelper.getWritableDatabase();
     }
 
-    @After
-    public void finish() {
-        mHelper.close();
-    }
+//    @After
+//    public void finish() {
+//
+//    }
 
     @Test
     public void testPreConditions() {
@@ -43,21 +48,64 @@ public class DatabaseInstrumentedTest {
 
     @Test
     public void testInsertAuthentication() {
-        mHelper.insertAuthentication(mDatabase, R.drawable.smartphone, R.drawable.fingerprintscan, R.drawable.confused, "took ages", "Home");
+        // mHelper.insertAuthentication(mWritableDatabase, R.drawable.smartphone, R.drawable.fingerprintscan, R.drawable.confused, "took ages", "Home");
+
+        ContentValues authenticationValues = new ContentValues();
+        authenticationValues.put(mHelper.DEVICE, R.drawable.smartphone);
+        authenticationValues.put(mHelper.AUTHEN, R.drawable.fingerprintscan);
+        authenticationValues.put(mHelper.EMOTION, R.drawable.confused);
+        //authenticationValues.put(mHelper.ADDED_ON, System.currentTimeMillis());
+        authenticationValues.put(mHelper.COMMENTS, "took ages");
+        authenticationValues.put(mHelper.LOCATION, "Home");
+        mWritableDatabase.insert(mHelper.TABLE_NAME, null, authenticationValues);
+
+        ContentValues authenticationValues2 = new ContentValues();
+        authenticationValues2.put(mHelper.DEVICE, R.drawable.car);
+        authenticationValues2.put(mHelper.AUTHEN, R.drawable.fingerprintscan);
+        authenticationValues2.put(mHelper.EMOTION, R.drawable.sad);
+        //authenticationValues.put(mHelper.ADDED_ON, System.currentTimeMillis());
+        authenticationValues2.put(mHelper.COMMENTS, "fine");
+        authenticationValues2.put(mHelper.LOCATION, "work");
+        mWritableDatabase.insert(mHelper.TABLE_NAME, null, authenticationValues2);
+
+        // returns -1 if database fails on inserting data otherwise, it will return the row number.
+        assertFalse(id == -1);
 
         String getAllDataQuery = "SELECT * FROM " + mHelper.TABLE_NAME;
 
         Log.i("getAllDataQuery", getAllDataQuery);
 
-        Cursor cursor = mDatabase.rawQuery(getAllDataQuery, null);
+        Cursor cursor = mReadableDatabase.rawQuery(getAllDataQuery, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+            int deviceId = cursor.getInt(1);
+            Log.i("id", "deviceID: " + deviceId);
+            //assertEquals(deviceId, R.drawable.smartphone);
+            Date c = new Date(cursor.getLong(5));
+            SimpleDateFormat ft = new SimpleDateFormat("E dd.MM.yyyy 'at' hh:mm:ss zz");
+            Log.i("time", ft.format(c));
 
-        if (cursor.moveToFirst()) {
-
-            int deviceId = cursor.getInt(0);
-            assertEquals(deviceId, R.drawable.smartphone);
         }
         cursor.close();
-        mDatabase.close();
+//        mHelper.close();
+//        mReadableDatabase.close();
+//        mWritableDatabase.close();
     }
 
+    @Test
+    public void getAllRecords() {
+        String getAllRows = "SELECT COUNT(*) FROM" + mHelper.TABLE_NAME;
+
+        Cursor cursor = mReadableDatabase.rawQuery(getAllRows, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+            int count = cursor.getInt(0);
+            Log.i("count:", count + "");
+            cursor.close();
+//            mHelper.close();
+//            mReadableDatabase.close();
+        }
+    }
 }
+
+
--- app/src/main/java/com/example/fraser/notaficationprototype/DatabaseHelper.java
+++ app/src/main/java/com/example/fraser/notaficationprototype/DatabaseHelper.java
@@ -4,6 +4,7 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by Fraser on 13/12/2016.
@@ -11,8 +12,17 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    private static final String DB_NAME = "AuthenticationDiary";
-    private static final int DB_VERSION = 1;
+    //public for unit testing
+
+    public static final String DB_NAME = "AuthenticationDiary";
+    public static final int DB_VERSION = 1;
+    public static final String TABLE_NAME = "AUTHENTICATION";
+    public static final String DEVICE = "DEVICE_RESOURCE_ID";
+    public static final String AUTHEN = "_AUTHENTICATOR_RESOURCE_ID";
+    public static final String LOCATION = "LOCATION";
+    public static final String COMMENTS = "COMMENTS";
+    public static final String EMOTION = "EMOTION_RESOURCE_ID";
+    public static final String ADDED_ON = "ADDED_ON";
 
 
     public DatabaseHelper(Context context) {
@@ -21,26 +31,29 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
         updateDatabase(db, 0, DB_VERSION);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+//        updateDatabase(db,oldVersion,newVersion);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
     }
 
     public void updateDatabase(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        Log.i("version",oldVersion+"");
         if (oldVersion < 1) {
-            db.execSQL("CREATE TABLE AUTHENTICATION (_id, INTEGER PRIMARY KEY AUTOINCREMENT, "
-                    + "DEVICE_RESOURCE_ID INTEGER"
-                    + "AUTHENTICATOR_RESOURCE_ID INTEGER"
-                    + "EMOTION_RESOURCE_ID INTEGER "
-                    + "COMMENTS TEXT"
-                    + "ADDED_ON TIMESTAMP NOT NULL DEFAULT current_timestamp"
-                    + "LOCATION TEXT");
-            insertAuthentication(db, R.drawable.smartphone, R.drawable.fingerprintscan, R.drawable.confused, "took ages", "Home");
+            db.execSQL("CREATE TABLE " + TABLE_NAME + " (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + DEVICE + " INTEGER, "
+                    + AUTHEN + " INTEGER, "
+                    + EMOTION + " INTEGER, "
+                    + COMMENTS + " TEXT, "
+                    + ADDED_ON+ "TIMESTAMP NOT NULL DEFAULT current_timestamp, "
+                    + LOCATION + " TEXT);");
+            //insertAuthentication(db, R.drawable.smartphone, R.drawable.fingerprintscan, R.drawable.confused, "took ages", "Home");
+        }else{
+            Log.d("other","version");
         }
     }
 
@@ -52,7 +65,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         authenticationValues.put("EMOTION_RESOURCE_ID", emotionID);
         authenticationValues.put("COMMENTS", comments);
         authenticationValues.put("LOCATION", location);
-        db.insert(DB_NAME, null, authenticationValues);
+        db.insert(TABLE_NAME, null, authenticationValues);
     }
 
     public void getAllAuthentications() {
