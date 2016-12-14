9e73553d7f2c3ffdee5c15550afbb2f2cb16608f
fraser0319 <fraser0319@gmail.com>
2016-12-14 13:12:36 +0000

    got gradle working and running for instrumented tests so commiting now before it breaks

--- app/src/androidTest/java/com/example/fraser/notaficationprototype/DatabaseInstrumentedTest.java
+++ app/src/androidTest/java/com/example/fraser/notaficationprototype/DatabaseInstrumentedTest.java
@@ -1,23 +1,25 @@
 package com.example.fraser.notaficationprototype;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 
-@RunWith(AndroidJUnit4.class)
 
 /**
  * Created by Fraser on 13/12/2016.
  */
-
-public class DatabaseUnitTest {
+@RunWith(AndroidJUnit4.class)
+public class DatabaseInstrumentedTest {
 
     private DatabaseHelper mHelper;
     private SQLiteDatabase mDatabase;
@@ -30,18 +32,32 @@ public class DatabaseUnitTest {
     }
 
     @After
-    public void finish(){
+    public void finish() {
         mHelper.close();
     }
 
     @Test
-    public void testPreConditions(){
+    public void testPreConditions() {
         assertNotNull(mHelper);
     }
 
     @Test
-    public void testInsertAuthentication(){
+    public void testInsertAuthentication() {
         mHelper.insertAuthentication(mDatabase, R.drawable.smartphone, R.drawable.fingerprintscan, R.drawable.confused, "took ages", "Home");
+
+        String getAllDataQuery = "SELECT * FROM " + mHelper.TABLE_NAME;
+
+        Log.i("getAllDataQuery", getAllDataQuery);
+
+        Cursor cursor = mDatabase.rawQuery(getAllDataQuery, null);
+
+        if (cursor.moveToFirst()) {
+
+            int deviceId = cursor.getInt(0);
+            assertEquals(deviceId, R.drawable.smartphone);
+        }
+        cursor.close();
+        mDatabase.close();
     }
 
 }
