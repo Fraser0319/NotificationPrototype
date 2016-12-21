f31788738854f00adbd734d3a5bc4bbeaf9a8ff0
fraser0319 <fraser0319@gmail.com>
2016-12-21 13:50:18 +0000

    changed the design a little and now dont need the dialog pop up for the addinng a comment, going to work on a detailed view fragment now instead

--- app/src/main/java/com/example/fraser/notaficationprototype/DetailedViewFragment.java
+++ app/src/main/java/com/example/fraser/notaficationprototype/DetailedViewFragment.java
@@ -11,10 +11,10 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class DetailViewFragment extends Fragment {
+public class DetailedViewFragment extends Fragment {
 
 
-    public DetailViewFragment() {
+    public DetailedViewFragment() {
         // Required empty public constructor
     }
 
@@ -23,7 +23,7 @@ public class DetailViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_view, container, false);
+        return inflater.inflate(R.layout.fragment_detailed_view, container, false);
     }
 
 }
