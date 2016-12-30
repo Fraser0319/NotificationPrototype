cbd7af2acd3cf822e7136270a681f3e1d451414e
fraser0319 <fraser0319@gmail.com>
2016-12-30 18:57:16 +0000

    updated to use services instead of broadcast recivcer and works while app is manually closed along with while the device is in the lock screen

--- app/src/main/java/com/example/fraser/notaficationprototype/StartFragment.java
+++ app/src/main/java/com/example/fraser/notaficationprototype/StartFragment.java
@@ -6,12 +6,14 @@ import android.app.FragmentTransaction;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
+import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
 import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -63,7 +65,6 @@ public class StartFragment extends Fragment {
     }
 
 
-
     public void startNotification(View v) {
         Button startNotification = (Button) v.findViewById(R.id.startButton);
         startNotification.setOnClickListener(new View.OnClickListener() {
@@ -71,7 +72,10 @@ public class StartFragment extends Fragment {
             public void onClick(View v) {
                 Toast t = Toast.makeText(v.getContext(), "yay button click", Toast.LENGTH_LONG);
                 t.show();
-                sendNotification();
+                Intent service = new Intent(getActivity(),ForegroundSerivice.class);
+                service.setAction("startForeground");
+                getActivity().startService(service);
+                //sendNotification();
             }
         });
 
@@ -83,6 +87,9 @@ public class StartFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 manager.cancel(1);
+                Intent intent = new Intent(getActivity(),ForegroundSerivice.class);
+                getActivity().stopService(intent);
+                Toast.makeText(getActivity(),"service stoped",Toast.LENGTH_LONG).show();
             }
         });
     }
@@ -103,58 +110,7 @@ public class StartFragment extends Fragment {
         });
     }
 
-    public void setupButtons() {
-        Collections.addAll(emotionList, R.drawable.happy, R.drawable.sad, R.drawable.confused);
-        Collections.addAll(authenList, R.drawable.password, R.drawable.fingerprintscan, R.drawable.cursor, R.drawable.hand_gesture, R.drawable.id_card, R.drawable.key, R.drawable.contract, R.drawable.locked, R.drawable.ticket);
-        Collections.addAll(devList, R.drawable.suv, R.drawable.metro, R.drawable.smartphone, R.drawable.mobile_phone, R.drawable.laptop, R.drawable.tramway, R.drawable.point_of_service, R.drawable.buses, R.drawable.atm, R.drawable.browser, R.drawable.locker, R.drawable.cycle);
 
-        ButtonChangeReceiver bcr = new ButtonChangeReceiver();
-        bcr.updateButton(emotionList, 0, R.id.emotionButton);
-        bcr.updateButton(authenList, 0, R.id.authenticatorButton);
-        bcr.updateButton(devList, 0, R.id.deviceButton);
-    }
-
-    @SuppressWarnings("deprecation")
-    public void sendNotification() {
-        authenticationDatabase = new DatabaseHelper(getActivity());
-        db = authenticationDatabase.getWritableDatabase();
-
-        Intent notificationIntent = new Intent(getActivity(), MainActivity.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(getActivity(), 0, notificationIntent, 0);
-
-        contentView = new RemoteViews(getActivity().getPackageName(), R.layout.notification_view);
-        contentView.setTextViewText(R.id.title, "title");
-
-        // add intents for each button to rotate through each image.
-        Intent changeEmotion = new Intent("changeEmotion");
-        PendingIntent pendingChangeEmotion = PendingIntent.getBroadcast(getActivity(), 0, changeEmotion, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        Intent changeAuthen = new Intent("changeAuthen");
-        PendingIntent pendingChangeAuthen = PendingIntent.getBroadcast(getActivity(), 0, changeAuthen, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        Intent changeDevice = new Intent("changeDevice");
-        PendingIntent pendingChangeDevice = PendingIntent.getBroadcast(getActivity(), 0, changeDevice, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        Intent confirmSelection = new Intent("confirm");
-        PendingIntent pendingConfirmSelection = PendingIntent.getBroadcast(getActivity(), 0, confirmSelection, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        // build the notification
-        notification = new NotificationCompat.Builder(getActivity())
-                .setSmallIcon(R.drawable.diary)
-                .setContent(contentView)
-                .setContentIntent(contentIntent)
-                .setOngoing(true)
-                .setContentTitle("custom View").build();
-        // add onclickpending intent listeners for each button
-        contentView.setOnClickPendingIntent(R.id.emotionButton, pendingChangeEmotion);
-        contentView.setOnClickPendingIntent(R.id.authenticatorButton, pendingChangeAuthen);
-        contentView.setOnClickPendingIntent(R.id.deviceButton, pendingChangeDevice);
-        contentView.setOnClickPendingIntent(R.id.confirmBtn, pendingConfirmSelection);
-        notification.contentView = contentView;
-        manager = (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
-        manager.notify(1, notification);
-        setupButtons();
-    }
 
 //    @Override
 //    public void onDestroyView() {
@@ -163,12 +119,20 @@ public class StartFragment extends Fragment {
 //    }
 
 
-    public static class ButtonChangeReceiver extends BroadcastReceiver {
+    public static class ForegroundSerivice extends Service {
 
         @Override
-        public void onReceive(Context context, Intent intent) {
-           //context.deleteDatabase("AuthenticationDiary");
+        public void onCreate() {
+            super.onCreate();
+        }
+
+        @Override
+        public int onStartCommand(Intent intent, int flags, int startId) {
             switch (intent.getAction()) {
+
+                case "startForeground" :
+                    sendNotification();
+                    Toast.makeText(this,"service started",Toast.LENGTH_LONG).show();
                 case "changeEmotion":
                     if (emotionCounter == emotionList.size() - 1) {
                         emotionCounter = 0;
@@ -198,10 +162,93 @@ public class StartFragment extends Fragment {
                     break;
                 case "confirm":
                     Log.e("here", "confirm");
-                    sendIntent(context);
+                    sendIntent();
                     break;
-
             }
+            return START_STICKY;
+        }
+
+        public void sendIntent() {
+            Bundle bundle = new Bundle();
+            bundle.putInt("emotionButton", getCurrentButton(emotionList, emotionCounter));
+            bundle.putInt("authenButton", getCurrentButton(authenList, authenCounter));
+            bundle.putInt("devButton", getCurrentButton(devList, deviceCounter));
+//
+////            Fragment summaryFragment = new SummaryFragment();
+////            summaryFragment.setArguments(bundle);
+////            FragmentTransaction ft =
+////            ft.replace(R.id.activity_main, summaryFragment);
+////            ft.addToBackStack(null);
+////            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+////            ft.commitAllowingStateLoss();
+
+            dbHelper.insertAuthentication(db, getCurrentButton(devList, deviceCounter), getCurrentButton(authenList, authenCounter), getCurrentButton(emotionList, emotionCounter), null, null);
+            Intent intent = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS); // close notification drawer after added
+            this.sendBroadcast(intent);
+            Toast.makeText(this,"Authentication Added !!",Toast.LENGTH_LONG).show();
+
+        }
+
+        public void setupButtons() {
+            Collections.addAll(emotionList, R.drawable.happy, R.drawable.sad, R.drawable.confused);
+            Collections.addAll(authenList, R.drawable.password, R.drawable.fingerprintscan, R.drawable.cursor, R.drawable.hand_gesture, R.drawable.id_card, R.drawable.key, R.drawable.contract, R.drawable.locked, R.drawable.ticket);
+            Collections.addAll(devList, R.drawable.suv, R.drawable.metro, R.drawable.smartphone, R.drawable.mobile_phone, R.drawable.laptop, R.drawable.tramway, R.drawable.point_of_service, R.drawable.buses, R.drawable.atm, R.drawable.browser, R.drawable.locker, R.drawable.cycle);
+
+
+            updateButton(emotionList, 0, R.id.emotionButton);
+            updateButton(authenList, 0, R.id.authenticatorButton);
+            updateButton(devList, 0, R.id.deviceButton);
+        }
+
+        @SuppressWarnings("deprecation")
+        public void sendNotification() {
+            authenticationDatabase = new DatabaseHelper(this);
+            db = authenticationDatabase.getWritableDatabase();
+
+            Intent notificationIntent = new Intent(this, MainActivity.class);
+            PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+            contentView = new RemoteViews(this.getPackageName(), R.layout.notification_view);
+            contentView.setTextViewText(R.id.title, "title");
+
+            // add intents for each button to rotate through each image.
+            Intent changeEmotion = new Intent(this,ForegroundSerivice.class);
+            changeEmotion.setAction("changeEmotion");
+            PendingIntent pendingChangeEmotion = PendingIntent.getService(this, 0, changeEmotion, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            Intent changeAuthen = new Intent(this,ForegroundSerivice.class);
+            changeAuthen.setAction("changeAuthen");
+            PendingIntent pendingChangeAuthen = PendingIntent.getService(this, 0, changeAuthen, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            Intent changeDevice = new Intent(this,ForegroundSerivice.class);
+            changeDevice.setAction("changeDevice");
+            PendingIntent pendingChangeDevice = PendingIntent.getService(this, 0, changeDevice, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            Intent confirmSelection = new Intent(this, ForegroundSerivice.class);
+            confirmSelection.setAction("confirm");
+            PendingIntent pendingConfirmSelection = PendingIntent.getService(this, 0, confirmSelection, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            // build the notification
+            notification = new NotificationCompat.Builder(this)
+                    .setSmallIcon(R.drawable.diary)
+                    .setContent(contentView)
+                    .setContentIntent(contentIntent)
+                    .setOngoing(true)
+                    .setContentTitle("custom View").build();
+            // add onclickpending intent listeners for each button
+            contentView.setOnClickPendingIntent(R.id.emotionButton, pendingChangeEmotion);
+            contentView.setOnClickPendingIntent(R.id.authenticatorButton, pendingChangeAuthen);
+            contentView.setOnClickPendingIntent(R.id.deviceButton, pendingChangeDevice);
+            contentView.setOnClickPendingIntent(R.id.confirmBtn, pendingConfirmSelection);
+            notification.contentView = contentView;
+            manager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+            manager.notify(1, notification);
+            setupButtons();
+            startForeground(1,notification);
+        }
+
+        public int getCurrentButton(ArrayList<Integer> imageList, int counter) {
+            return imageList.get(counter);
         }
 
         @SuppressWarnings("deprecation")
@@ -215,32 +262,96 @@ public class StartFragment extends Fragment {
             return false;
         }
 
-        public void sendIntent(Context context) {
-            Bundle bundle = new Bundle();
-            bundle.putInt("emotionButton", getCurrentButton(emotionList, emotionCounter));
-            bundle.putInt("authenButton", getCurrentButton(authenList, authenCounter));
-            bundle.putInt("devButton", getCurrentButton(devList, deviceCounter));
-
-//            Fragment summaryFragment = new SummaryFragment();
-//            summaryFragment.setArguments(bundle);
-//            FragmentTransaction ft =
-//            ft.replace(R.id.activity_main, summaryFragment);
-//            ft.addToBackStack(null);
-//            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-//            ft.commitAllowingStateLoss();
-
-            dbHelper.insertAuthentication(db,getCurrentButton(devList, deviceCounter),getCurrentButton(authenList, authenCounter), getCurrentButton(emotionList, emotionCounter),null,null);
-            Intent intent = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS); // close notification drawer after added
-            context.sendBroadcast(intent);
-
-            Toast.makeText(context,"Authentication Added !!",Toast.LENGTH_LONG).show();
-
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            Toast.makeText(this, "Service Detroyed!", Toast.LENGTH_SHORT).show();
         }
 
-        public int getCurrentButton(ArrayList<Integer> imageList, int counter) {
-            return imageList.get(counter);
-        }
 
+        @Nullable
+        @Override
+        public IBinder onBind(Intent intent) {
+            return null;
+        }
     }
-
 }
+
+
+//        @Override
+//        public void onReceive(Context context, Intent intent) {
+//           //context.deleteDatabase("AuthenticationDiary");
+//            switch (intent.getAction()) {
+//                case "changeEmotion":
+//                    if (emotionCounter == emotionList.size() - 1) {
+//                        emotionCounter = 0;
+//                        updateButton(emotionList, emotionCounter, R.id.emotionButton);
+//                    } else {
+//                        emotionCounter++;
+//                        updateButton(emotionList, emotionCounter, R.id.emotionButton);
+//                    }
+//                    break;
+//                case "changeAuthen":
+//                    if (authenCounter == authenList.size() - 1) {
+//                        authenCounter = 0;
+//                        updateButton(authenList, authenCounter, R.id.authenticatorButton);
+//                    } else {
+//                        authenCounter++;
+//                        updateButton(authenList, authenCounter, R.id.authenticatorButton);
+//                    }
+//                    break;
+//                case "changeDevice":
+//                    if (deviceCounter == devList.size() - 1) {
+//                        deviceCounter = 0;
+//                        updateButton(devList, deviceCounter, R.id.deviceButton);
+//                    } else {
+//                        deviceCounter++;
+//                        updateButton(devList, deviceCounter, R.id.deviceButton);
+//                    }
+//                    break;
+//                case "confirm":
+//                    Log.e("here", "confirm");
+//                    sendIntent(context);
+//                    break;
+//
+//            }
+//        }
+//
+//        @SuppressWarnings("deprecation")
+//        public boolean updateButton(ArrayList<Integer> imageList, int counter, int buttonID) {
+//            if (counter < imageList.size()) {
+//                contentView.setImageViewResource(buttonID, imageList.get(counter));
+//                notification.contentView = contentView;
+//                manager.notify(1, notification);
+//                return true;
+//            }
+//            return false;
+//        }
+//
+//        public void sendIntent(Context context) {
+//            Bundle bundle = new Bundle();
+//            bundle.putInt("emotionButton", getCurrentButton(emotionList, emotionCounter));
+//            bundle.putInt("authenButton", getCurrentButton(authenList, authenCounter));
+//            bundle.putInt("devButton", getCurrentButton(devList, deviceCounter));
+//
+////            Fragment summaryFragment = new SummaryFragment();
+////            summaryFragment.setArguments(bundle);
+////            FragmentTransaction ft =
+////            ft.replace(R.id.activity_main, summaryFragment);
+////            ft.addToBackStack(null);
+////            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+////            ft.commitAllowingStateLoss();
+//
+//            dbHelper.insertAuthentication(db,getCurrentButton(devList, deviceCounter),getCurrentButton(authenList, authenCounter), getCurrentButton(emotionList, emotionCounter),null,null);
+//            Intent intent = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS); // close notification drawer after added
+//            context.sendBroadcast(intent);
+//
+//            Toast.makeText(context,"Authentication Added !!",Toast.LENGTH_LONG).show();
+//
+//        }
+//
+//        public int getCurrentButton(ArrayList<Integer> imageList, int counter) {
+//            return imageList.get(counter);
+//        }
+
+
